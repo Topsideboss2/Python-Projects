d2fdbf3100b5f0abc4dde59c16d3f1a83d042a83
Mark Kibara <markkibara@Marks-MacBook-Air.local>
2023-01-08 00:21:04 +0300

    "Modified fibo.py"

--- fibo.py
+++ fibo.py
@@ -1,7 +1,7 @@
 def fib(n):
     a, b = 0, 1
     while a < n:
-        print(a, end='')
+        print(a, end=' ')
         a, b = b, a + b
     print()
 
@@ -12,3 +12,7 @@ def fib2(n):
         result.append(a)
         a, b = b, a + b
     return result
+
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
