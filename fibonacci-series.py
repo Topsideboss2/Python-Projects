a376b32191fcf2adb0a255ca10af1b080f4fb2cc
Mark Kibara <markkibara@Marks-MacBook-Air.local>
2023-01-08 23:33:05 +0300

    Modified fibonacci-series.py

--- fibonacci-series.py
+++ fibonacci-series.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def fib(n):
-	"""Print a Fibonacci series up to n."""
-	a, b = 0, 1
-	while a < n:
-		print(a, end=' ')
-		a, b = b, a+b
-	print()
+    """Print a Fibonacci series up to n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
-fib(2000)
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
